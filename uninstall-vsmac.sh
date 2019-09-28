5487df5bb154d977f4080b9e815c447af55f53dc
Philipp Schunker <philipp@Philipps-MacBook-Pro.local>
2019-09-28 09:22:02 +0200

    updated uninstall-vsmac.sh

--- uninstall-vsmac.sh
+++ uninstall-vsmac.sh
@@ -9,6 +9,7 @@ rm -rf ~/Library/Preferences/VisualStudio
 rm -rf ~/Library/Preferences/Visual\ Studio
 rm -rf ~/Library/Logs/VisualStudio
 rm -rf ~/Library/VisualStudio
+rm -rf ~/Library/*Xamarin* 
 rm -rf ~/Library/Preferences/Xamarin/
 rm -rf ~/Library/Application\ Support/VisualStudio
 rm -rf ~/Library/Application\ Support/VisualStudio/7.0/LocalInstall/Addins/
