8fc54210c7834832fc7e762b3a24a4e4ebce5f10
Philipp Schunker <philipp@Philipps-MacBook-Pro.local>
2019-11-10 16:05:02 +0100

    updated ignore-macos-softwareupdate.sh

--- ignore-macos-softwareupdate.sh
+++ ignore-macos-softwareupdate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# ignore-macos-softwareupdate.sh
+# sudo softwareupdate --reset-ignored
+
 sudo softwareupdate --ignore "macOS Catalina"
 #defaults write com.apple.systempreferences AttentionPrefBundleIDs 0
